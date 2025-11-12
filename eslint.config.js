4caf32cbba8d79eb285eb30ea41fd1503e7fa462
Abdullah  Faisal <lionking@192.168.0.155>
2025-11-13 02:29:07 +0500

    fix: Fix linting issues

--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,7 @@ import tseslint from "typescript-eslint";
 export default tseslint.config(
   {
     name: "global-ignores",
-    ignores: ["dist/**", "node_modules/**"],
+    ignores: ["dist/**", "node_modules/**", "functions/functions/lib/**"],
   },
   js.configs.recommended,
   ...tseslint.configs.recommended,
